bff9bbb1875456c2e68e363f5c1b1ecee561a4e8
Pool Ramos <poolramos.ayacucho@gmail.com>
2021-09-09 23:59:12 -0500

    desafío API Restful

---
+++ API Restful/desafio.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const Productos = require('./productos');
+
+const puerto = 8080;
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/', express.static(__dirname + '/public'))
+const routerGlobal = express.Router();
+
+
+//GET listado 
+routerGlobal.get('/productos',(req, res) => {
+    res.json(Productos.leerProductos())
+})
+//GET producto por ID 
+routerGlobal.get('/productos/:id',(req, res) => {
+    res.json(Productos.leerProductosConId(req.params.id))
+})
+//POST de un producto nuevo sin ID
+routerGlobal.post('/productos',(req,res) => {
+  let prodGuardado = Productos.productoNuevo(req.body)
+  res.send("Producto guardado")
+  res.send(prodGuardado)
+})
+//PUT de un producto nuevo con ID
+routerGlobal.put('/productos/:id',(req,res) => {
+  let prodNuevo = req.body;
+  let idProdNuevo = req.params.id
+  let prodActualizado = Productos.actualizarConID(idProdNuevo, prodNuevo)
+  res.send("Producto actualizado")
+  res.send(prodActualizado)
+})
+//DELETE de un producto con ID
+routerGlobal.delete('/productos/:id',(req,res) => {
+  let idProdABorrar = req.params.id
+  let prodBorrado = Productos.borrarConID(idProdABorrar)
+  res.send("Producto eliminado")
+  res.send(prodBorrado)
+})
+
+// aplico el router global
+app.use('/api',routerGlobal)
+
+const server = app.listen(puerto, () => {
+    console.log(`servidor escuchando en http://localhost:${puerto}`);
+});
+
+server.on('error', error => {
+    console.log('Error en el servidor:', error);
+});
